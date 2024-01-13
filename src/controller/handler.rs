7035941a68050c83bd8a351948af5810f157870f
ryo <nakayaryo0716@gmail.com>
2024-01-14 00:15:50 +0900

    completed write moc model

--- src/controller/handler.rs
+++ src/controller/handler.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
-use axum::{response::{IntoResponse, Html}, Json, extract::State};
+use axum::{response::{IntoResponse, Html}, Json, extract::{State, Path}};
 use hyper::StatusCode;
-use crate::model::{data_type::CreateTodo, repository::TodoRepositoryForMemory};
+use crate::model::{data_type::{CreateTodo, UpdateTodo}, repository::TodoRepositoryForMemory};
 
 pub async fn hello() -> impl IntoResponse {
     Html("<h1>Hello World!</h1>")
@@ -14,4 +14,46 @@ pub async fn create_todo<T: TodoRepositoryForMemory>(
     let todo = repository.create(payload);
 
     (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepositoryForMemory>(
+    State(repository): State<Arc<T>>,
+    Path(id): Path<i32> 
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .find(id)
+        .or(Err(StatusCode::NOT_FOUND))?;
+
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn all_todo<T: TodoRepositoryForMemory>(
+    State(repository): State<Arc<T>>
+) -> impl IntoResponse {
+    let todo = repository.all();
+
+    (StatusCode::OK, Json(todo))
+}
+
+pub async fn update_todo<T: TodoRepositoryForMemory>(
+    State(repository): State<Arc<T>>,
+    Path(id): Path<i32>,
+    Json(payload): Json<UpdateTodo>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .update(id, payload)
+        .or(Err(StatusCode::NOT_FOUND))?;
+
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn delete_todo<T: TodoRepositoryForMemory>(
+    State(repository): State<Arc<T>>,
+    Path(id): Path<i32>
+) -> Result<impl IntoResponse, StatusCode> {
+    let _todo = repository
+        .delete(id)
+        .or(Err(StatusCode::NOT_FOUND))?;
+
+    Ok(StatusCode::NO_CONTENT)
 }
--- src/route.rs
+++ src/route.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 
 use axum::{Router, routing::{get, post}};
-use crate::{controller::handler::{hello, create_todo}, model::repository::TodoRepositoryForMemory};
+use crate::{controller::handler::{hello, create_todo, all_todo, find_todo, delete_todo, update_todo}, model::repository::TodoRepositoryForMemory};
 
 pub fn app<T: TodoRepositoryForMemory>(repository: T) -> Router {
     Router::new()
         .route("/", get(hello))
-        .route("/todo", post(create_todo))
+        .route("/todo", post(create_todo).get(all_todo))
+        .route("/todo/:id", get(find_todo).put(update_todo).delete(delete_todo))
         .with_state(Arc::new(repository))
 }
