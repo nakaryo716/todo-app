425a0b97f82e02c163fa2a40fe18cf9780b82be0
ryonakaya <nakayaryo0716@gmail.com>
2024-02-10 14:25:16 +0900

    I have completed the development of this app

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import { Todo } from "../types/todo"
-import { Button, Card, Checkbox, Grid, Stack, Typography } from "@mui/material"
+import { Box, Button, Card, Checkbox, Grid, Modal, Stack, TextField, Typography } from "@mui/material"
+import { modalInnerStyle } from "../styles/modal"
 
 type Props = {
     todo: Todo
@@ -9,11 +10,26 @@ type Props = {
 }
 
 export const TodoItem: FC<Props> = ({todo, onUpdate, onDelete}) => {
+    const [editing, setEditing] = useState(false);
+    const [editText, setEditText] = useState('');
+
+    useEffect(() => {
+        setEditText(todo.text);
+    }, [todo])
+    
     const handleCompletedCheckbox = async () => {
         onUpdate({
             ...todo,
             completed: !todo.completed,
         })
+    }
+
+    const onCloseEditModal = async () => {
+        onUpdate({
+            ...todo,
+            text: editText,
+        })
+        setEditing(false);
     }
 
     const handleDelete = () => onDelete(todo.id)
@@ -24,19 +40,34 @@ export const TodoItem: FC<Props> = ({todo, onUpdate, onDelete}) => {
                 <Grid item xs={1}>
                     <Checkbox onChange={handleCompletedCheckbox} checked={todo.completed}/>
                 </Grid>
-                <Grid item xs={9}>
+                <Grid item xs={8}>
                     <Stack spacing={1}>
                         <Typography variant="caption" fontSize={16}>
                             {todo.text}
                         </Typography>
                     </Stack>
                 </Grid>
-                <Grid item xs={1}>
-                    <Button onClick={handleDelete} color="error">
-                        delete
-                    </Button>
+                <Grid item xs={2}>
+                    <Stack direction={"row"} spacing={1}>
+                        <Button onClick={() => setEditing(true)} color="info">edit</Button>
+                        <Button onClick={handleDelete} color="error">
+                            delete
+                        </Button>
+                    </Stack>
                 </Grid>
             </Grid>
+            <Modal open={editing} onClose={onCloseEditModal}>
+                <Box sx={modalInnerStyle}>
+                    <Stack spacing={2}>
+                        <TextField
+                            size="medium"
+                            label="todo text"
+                            defaultValue={todo.text}
+                            onChange={(e) => setEditText(e.target.value)}
+                        ></TextField>
+                    </Stack>
+                </Box>
+            </Modal>
         </Card>
     )
 }
---
+++ client/src/styles/modal.ts
@@ -0,0 +1,10 @@
+export const modalInnerStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    bgcolor: "background.paper",
+    boxShadow: 25,
+    P: 10,
+}
